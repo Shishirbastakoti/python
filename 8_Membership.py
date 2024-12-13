5f42a3f9f83144bcacf231262ef539ddaf1bf4df
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:37:51 +0545

    Add files via upload

---
+++ 8_Membership.py
@@ -0,0 +1,11 @@
+"""
+It gives result in the form True or False.
+It checks whether the value is in variable or not.
+"""
+
+x=["laptop", "operators","arithmetic", "logical"]
+print("laptop" in x)
+
+print("operators" not in x)
+
+print("apple" in x)
