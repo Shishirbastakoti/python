abfdf8be8abf271d0c0686f36bc819bf23b216e3
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:35:21 +0545

    Add files via upload

---
+++ 3_Assignment.py
@@ -0,0 +1,18 @@
+"""
+Assignment operator used to assign value to variables
+"""
+x=100 # 100 is assigned as value of x
+x+=3 #3 is added to assigned value of x
+print(x)
+
+x-=5
+print(x)
+
+x*=4
+print(x)
+
+x/=5
+print(x)
+
+x%=4
+print(x)
