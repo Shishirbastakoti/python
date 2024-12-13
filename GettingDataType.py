4f3a5d64db0f3e0b487c857d87e8805058e41fe6
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:22:32 +0545

    Add files via upload

---
+++ GettingDataType.py
@@ -0,0 +1,20 @@
+x=5
+print(type(x))    #type() function gives datatype of variable
+
+y="hello"
+print(type(y))
+
+z=True
+print(type(z))
+
+p=55.78
+print(type(p))
+
+q=("hello", "world", "python", "data", "science",5 )
+print(type(q))
+
+r=["apple", "oranges","ball"]
+print(type(r))
+
+s={1,2,3,4,5,"harry"}
+print(type(s))
