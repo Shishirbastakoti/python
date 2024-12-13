124dac060e378caebeb9e8f30f1df315b66c6011
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:24:42 +0545

    Add files via upload

---
+++ 1_string.py
@@ -0,0 +1,20 @@
+# string is kept in double quote " "
+
+a="this is string" #this is the method of printing single line string
+print(a)
+print(type(a)) #it prints type
+
+#way to print multi line string
+string="""hello i am shishir bastakoti.
+This is python and this is interesting.
+I am enjoying a lot"""
+print(string)
+
+#string can also be printed by single quote ''
+hello='window is too small'
+print(hello)
+
+rem='''hello i am shishir bastakoti.
+This is python and this is interesting.
+I am enjoying a lot'''
+print(rem)
