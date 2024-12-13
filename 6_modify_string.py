cb1afba9fda737c840cd162f3c7edf684790ce17
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:26:45 +0545

    Add files via upload

---
+++ 6_modify_string.py
@@ -0,0 +1,20 @@
+#to print in upper case
+a="meghraj poudel"
+x=a.upper()
+print(x)
+
+b="MEGHRAJ POUDEL"
+print(b.lower())
+
+#to remove white spaces
+#white space means space before and after actual text
+c="   amazing isn't it   "
+print(c.strip())
+
+#to replace string
+d="anaconda"
+print(d.replace("a","b"))
+
+#string split
+string="are you joking to me"
+print(string.split(","))
