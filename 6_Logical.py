520ded00482987ebdcff180a0b58cb4a57cb521b
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:37:32 +0545

    Add files via upload

---
+++ 6_Logical.py
@@ -0,0 +1,10 @@
+"""
+Logical operators perform logical operations such as OR, AND and NOT.
+It returns booelan values.
+"""
+x=10
+print(x<5 and x>4)
+
+print(x<5 or x>90)
+
+print(not(x<5 and x>78))
