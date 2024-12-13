dff3cc05a40ecda4bf4ef1a37141bbcca568d8c4
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:27:06 +0545

    Add files via upload

---
+++ 7_string_concatenate.py
@@ -0,0 +1,8 @@
+
+a="hello"
+b="world"
+#string can be concatenated by adding strings
+c=a+b   #merge variable a and b to variable c
+print(c) 
+print(a+" "+b)  # " " this prints a space between concatenated words
+
