ee511fe08303ac20d549782615aa06e56a41808c
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:35:02 +0545

    Add files via upload

---
+++ 2_Arithmetic.py
@@ -0,0 +1,43 @@
+"""
+Arithmetic operators use to perform mathematical calculation and operation.
+
+Arithmetic operators includes following operations:
+addition, subtraction, multiplication, division, modulus, exponentiation and floor division. 
+"""
+
+#addition
+print("Addition: ")
+num1=45
+num2=35
+sum=num1+num2
+print(sum)
+print("\n")
+
+#SUBTRACTION
+print("Subtraction: ")
+difference=num1-num2
+print(difference)
+print("\n")
+
+#DIVISION
+print("Division: ")
+division=num1/num2
+print(division)
+print("\n")
+
+#MODULUS
+print("Modulus: ")
+mod=num1%num2
+print(mod)
+print("\n")
+
+#exponentiation
+print("Exponentiation: ")
+exp=num1**num2
+print(exp)
+print("\n")
+
+#FLOOR DIVISION
+print("Floor Division: ")
+floor_div=num1//num2
+print(floor_div)
