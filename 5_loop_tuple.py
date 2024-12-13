3fcca7ec39a6fa33e7efb0d7aa3be4cc842a5872
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:43:13 +0545

    Add files via upload

---
+++ 5_loop_tuple.py
@@ -0,0 +1,19 @@
+"""
+for printing tuple items we can use for loop.
+"""
+
+fruits=("apple", "banana", "durian", "grapes", "pomegranate")
+for y in fruits:
+  print(y)
+
+for i in range (len(fruits)):
+  print(fruits[i])
+
+print("\n")
+#using while loop
+i=0
+while i<len(fruits):
+  print(fruits[i])
+  i=i+1
+
+
