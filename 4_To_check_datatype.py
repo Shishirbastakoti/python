96c274d9f9458619d0dc588e59027f3cb281a447
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:34:16 +0545

    Add files via upload

---
+++ 4_To_check_datatype.py
@@ -0,0 +1,17 @@
+x="hello"
+print(isinstance(x,str))
+print(isinstance(x,int))
+print(isinstance(x,float))
+print("\n")
+
+y=222
+print(isinstance(y,int))
+print(isinstance(y,float))
+print(isinstance(y,str))
+print("\n")
+
+z=5.5
+print(isinstance(z,float))
+print(isinstance(z,int))
+print(isinstance(z,str))
+print("\n")
