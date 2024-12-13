2d64b29d2b345581c109b8412d1a0180e7d50f9c
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:33:40 +0545

    Add files via upload

---
+++ 2_example1.py
@@ -0,0 +1,4 @@
+print(5>8) # 5 is less than 8 but here expression says 5 is greater than 8 so it returns false boolean value
+print(5==8) # 5 is  not equql to 8 so it returns FALSE boolean value
+print(6!=5) #6 is not equal to 5 so it returns TRUE boolean value
+print(10<100) #10 is less than 100 so it return TRUE boolean value
