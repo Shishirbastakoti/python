a2e17b43b2cb2b707a62b09ea3447d4f8bacc584
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:42:52 +0545

    Add files via upload

---
+++ 4_unpack_tuple.py
@@ -0,0 +1,11 @@
+"""
+we normally assign values to it. This is called "packing" a tuple.
+"""
+
+#packing tuple
+fruits=("apple", "banana", "grapes", "pomegranate")
+(a, b, c, d)=fruits
+print(a)
+print(b)
+print(c)
+print(d)
