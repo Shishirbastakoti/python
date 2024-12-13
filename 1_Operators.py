fa9186d6804d83433c98608b132da0fc4aaf01f9
Shishirbastakoti <129671168+Shishirbastakoti@users.noreply.github.com>
2024-12-13 18:34:40 +0545

    Add files via upload

---
+++ 1_Operators.py
@@ -0,0 +1,10 @@
+"""
+python has different operators.
+1.Arithmentic Operators
+2.Assignment operators
+3.Comparison operators
+4.Logical operators
+5.Identity operators
+6.Membership operators
+7.Bitwise operators
+"""
